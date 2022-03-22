c4ff02ae95fc9aff246894efdc841131e467ca68
Kashish Chawla <69769135+kashishchawla1@users.noreply.github.com>
2022-03-22 19:37:29 +0930

    order pizza

--- Pizza.py
+++ Pizza.py
@@ -19,3 +19,65 @@ pizzas_available = (
     {"name": "BBQ Hawaiian", "price": 13.5},
     {"name": "Italiano", "price": 13.5},
 )
+
+toppings = {
+    "Pepperoni":0.9,
+    "Mushrooms":1.6,
+    "Onions":0.2,
+    "Bacon":1.5,            
+    "Extra cheese":2.1,
+    "Black olives":1.7,
+    "Green pepper":2.1
+}
+
+sauces = {
+    "Tomato":1.9,
+    "Garlic":1.6,
+    "Chilly":3.2
+}
+
+class Order(object):
+
+    def __init__(self, name, address, pickup):
+        self.name = name;
+        self.address = address
+        self.pickup = pickup
+        self.pizzas = []
+        
+    def add_pizza(self, newPizza):
+        if(len(self.pizzas)>0):
+            for pizza in self.pizzas:
+                if(not pizza.get_name() == newPizza.get_name()):
+                    self.pizzas.append(newPizza)
+        else:
+            self.pizzas.append(newPizza)
+    def remove_pizza(self, name):
+        for pizza in self.pizzas:
+                if(pizza.get_name() == name):
+                    del pizza
+                    
+    def get_pizza(self, name):
+        for pizza in self.pizzas:
+                if(pizza.get_name() == name):
+                    return pizza;
+        return None
+    def get_pizzas(self):
+        return self.pizzas
+    
+    def get_address(self):
+        return self.address
+    
+    def get_name(self):
+        return self.name
+    
+    def is_pickup(self):
+        return self.pickup
+
+    def set_name(self, name):
+        self.name = name
+    
+    def set_address(self, address):
+        self.address = address
+    
+    def set_pickup(self, pickup):
+        self.pickup = pickup
